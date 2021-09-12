cba39011d0a4a9632dd5462e79389b2640cd4dc7
jotag7 <55748020+jotag7@users.noreply.github.com>
2021-09-12 02:34:03 -0500

    prueba web

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+  $("#lightSlider").lightSlider(); 
+});
+
+
+document.getElementById("exp1").addEventListener("mouseover, function (event) {
+        alert("Hola a todos111");
+        
+});
